e9e3f004781c47681256700914dc07f097fe8b67
ShivamBhosale <shivambhosale1511@gmail.com>
2023-12-13 19:51:31 -0500

    User Registration added

--- ranthub/ranthubapp/urls.py
+++ ranthub/ranthubapp/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('login/', views.loginPage, name='login'),
     path('logout/', views.logoutUser, name='logout'),
     path('', views.home, name='home'),
+    path('register/', views.registerPage, name='register'),
     path('room/<str:pk>/', views.room, name='room'),
     path('create-room/', views.createRoom, name='create-room'),
     path('update-room/<str:pk>/', views.updateRoom, name='update-room'),
